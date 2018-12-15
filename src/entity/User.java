0b15eb41a51c82d59f7e40d0ca4b2167b7bbc0b6
bluohazard <greggygf@gmail.com>
2018-12-15 10:50:50 +0700

    beautify <3

--- src/entity/User.java
+++ src/entity/User.java
@@ -9,19 +9,23 @@ public class User
     {
     }
     
-    public String getUsername() {
+    public String getUsername() 
+    {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(String username) 
+    {
         this.username = username;
     }
 
-    public String getPassword() {
+    public String getPassword() 
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) 
+    {
         this.password = password;
     }
 }
