db37af1ca68f66ec6ca0489d5a4e7559f4e663c9
bluohazard <greggygf@gmail.com>
2018-12-22 18:44:56 +0700

    add dao for select,selectwhere

--- src/dao/JenisLaundryDao.java
+++ src/dao/JenisLaundryDao.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import lib.ManajerKoneksi;
 
 public class JenisLaundryDao 
@@ -43,5 +44,48 @@ public class JenisLaundryDao
         Statement s = this.koneksi.createStatement();
 
         s.executeUpdate(sql);
+    }
+    
+    public ArrayList<JenisLaundry> ambilSemuaData() throws SQLException
+    {
+        ArrayList<JenisLaundry> semuanya = this.selectWhere(null);
+        
+        return semuanya;
+    }
+    
+    private ArrayList<JenisLaundry> selectWhere(String where) throws SQLException
+    {
+        String sql = "SELECT * FROM jenis_laundry";
+        
+        if(where != null)
+            sql += ("" + where);
+        
+        try
+        {
+            // Menjalankan SQL
+            Statement s = this.koneksi.createStatement();
+        
+            ResultSet hasil = s.executeQuery(sql);
+        
+            // Siapkan array kosong untuk menyimpan mahasiswa dari ResultSet
+            ArrayList<JenisLaundry> jlTerpilih = new ArrayList<>();
+
+            while(hasil.next())
+            {
+                JenisLaundry jl = new JenisLaundry();
+                jl.setId_jenis_laundry(hasil.getInt("id_jenis_laundry"));
+                jl.setNama_jenis_laundry(hasil.getString("nama_jenis_laundry"));
+
+                // Memasukkan objek m kedalam array list
+                jlTerpilih.add(jl);
+            }
+            
+            return jlTerpilih;
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("Select where error : " + ex.getMessage());
+            return null;
+        }
     }
 }
