370e51590c1741252416df6002fcdcb305226c23
bluohazard <greggygf@gmail.com>
2018-12-22 18:39:54 +0700

    add select,select where

--- src/dao/CustomerDao.java
+++ src/dao/CustomerDao.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import lib.ManajerKoneksi;
 
 public class CustomerDao 
@@ -45,5 +46,50 @@ public class CustomerDao
         Statement s = this.koneksi.createStatement();
 
         s.executeUpdate(sql);
+    }
+    
+    public ArrayList<Customer> ambilSemuaData() throws SQLException
+    {
+        ArrayList<Customer> semuanya = this.selectWhere(null);
+        
+        return semuanya;
+    }
+    
+    private ArrayList<Customer> selectWhere(String where) throws SQLException
+    {
+        String sql = "SELECT * FROM customer";
+        
+        if(where != null)
+            sql += ("" + where);
+        
+        try
+        {
+            // Menjalankan SQL
+            Statement s = this.koneksi.createStatement();
+        
+            ResultSet hasil = s.executeQuery(sql);
+        
+            // Siapkan array kosong untuk menyimpan mahasiswa dari ResultSet
+            ArrayList<Customer> customerTerpilih = new ArrayList<>();
+
+            while(hasil.next())
+            {
+                Customer c = new Customer();
+                c.setId_customer(hasil.getInt("id_customer"));
+                c.setNama(hasil.getString("nama"));
+                c.setAlamat(hasil.getString("alamat"));
+                c.setNoTelp(hasil.getString("no_telp"));
+
+                // Memasukkan objek m kedalam array list
+                customerTerpilih.add(c);
+            }
+            
+            return customerTerpilih;
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("Select where error : " + ex.getMessage());
+            return null;
+        }
     }
 }
