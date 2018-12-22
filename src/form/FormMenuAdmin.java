d69e9f1f4a557cda918c141471cfe3f25c2b4aba
bluohazard <greggygf@gmail.com>
2018-12-22 13:25:25 +0700

    modified action button add customer

--- src/form/FormMenuAdmin.java
+++ src/form/FormMenuAdmin.java
@@ -198,7 +198,8 @@ public class FormMenuAdmin extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        dispose();
+        new FormAddCustomer().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
