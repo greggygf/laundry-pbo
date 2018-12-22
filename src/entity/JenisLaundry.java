e6f0172e393ab4d8a8d627914095ebea51bda77d
bluohazard <greggygf@gmail.com>
2018-12-22 13:55:18 +0700

    modified, add class getter setter

--- src/entity/JenisLaundry.java
+++ src/entity/JenisLaundry.java
@@ -1,11 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package entity;
 
-public class JenisLaundry {
+public class JenisLaundry 
+{
+    private int id_jenis_laundry;
+    private String nama_jenis_laundry;
 
+    public int getId_jenis_laundry() 
+    {
+        return id_jenis_laundry;
+    }
+
+    public void setId_jenis_laundry(int id_jenis_laundry) 
+    {
+        this.id_jenis_laundry = id_jenis_laundry;
+    }
+
+    public String getNama_jenis_laundry() 
+    {
+        return nama_jenis_laundry;
+    }
+
+    public void setNama_jenis_laundry(String nama_jenis_laundry) 
+    {
+        this.nama_jenis_laundry = nama_jenis_laundry;
+    }
+    
+    
 }
