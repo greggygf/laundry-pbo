560fb131bc3efd8ab33da2edf570c3d77d63275e
bluohazard <greggygf@gmail.com>
2018-12-23 01:17:14 +0700

    modify

--- src/entity/Transaksi.java
+++ src/entity/Transaksi.java
@@ -1,5 +1,67 @@
 package entity;
 
-public class Transaksi {
+public class Transaksi 
+{
+    private int id_transaksi, id_tarif, berat,total;
+    private String nama, tgl;
 
+    public int getId_transaksi() 
+    {
+        return id_transaksi;
+    }
+
+    public void setId_transaksi(int id_transaksi) 
+    {
+        this.id_transaksi = id_transaksi;
+    }
+
+    public int getId_tarif() 
+    {
+        return id_tarif;
+    }
+
+    public void setId_tarif(int id_tarif)
+    {
+        this.id_tarif = id_tarif;
+    }
+
+    public int getBerat() 
+    {
+        return berat;
+    }
+
+    public void setBerat(int berat) 
+    {
+        this.berat = berat;
+    }
+
+    public int getTotal() 
+    {
+        return total;
+    }
+
+    public void setTotal(int total)
+    {
+        this.total = total;
+    }
+
+    public String getNama() 
+    {
+        return nama;
+    }
+
+    public void setNama(String nama) 
+    {
+        this.nama = nama;
+    }
+
+    public String getTgl() 
+    {
+        return tgl;
+    }
+
+    public void setTgl(String tgl) 
+    {
+        this.tgl = tgl;
+    }
 }
