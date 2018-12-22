31ee4fe0150e37d5ef0453455c1b1901170e866e
bluohazard <greggygf@gmail.com>
2018-12-22 14:38:56 +0700

    add class, add getter setter

--- src/entity/JenisPakaian.java
+++ src/entity/JenisPakaian.java
@@ -1,5 +1,25 @@
 package entity;
 
-public class JenisPakaian {
+public class JenisPakaian 
+{
+    private int id_jenis_pakaian;
+    private String nama_jenis_pakaian;
 
+    public int getId_jenis_pakaian() {
+        return id_jenis_pakaian;
+    }
+
+    public void setId_jenis_pakaian(int id_jenis_pakaian) {
+        this.id_jenis_pakaian = id_jenis_pakaian;
+    }
+
+    public String getNama_jenis_pakaian() {
+        return nama_jenis_pakaian;
+    }
+
+    public void setNama_jenis_pakaian(String nama_jenis_pakaian) {
+        this.nama_jenis_pakaian = nama_jenis_pakaian;
+    }
+    
+    
 }
