b7b291ac53ac08d0122558030ee7c5564978fa4a
bluohazard <greggygf@gmail.com>
2018-12-22 15:57:51 +0700

    modified function countID and procedure insert

--- src/dao/CustomerDao.java
+++ src/dao/CustomerDao.java
@@ -1,11 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package dao;
 
-public class CustomerDao {
+import entity.Customer;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import lib.ManajerKoneksi;
 
+public class CustomerDao 
+{
+    private final Connection koneksi;
+
+    public CustomerDao() 
+    {
+        this.koneksi = ManajerKoneksi.getKoneksi();
+    }
+    
+    public int countID() throws SQLException 
+    {
+        String sql = "select * from customer";
+
+        Statement s = this.koneksi.createStatement();
+
+        ResultSet hasil = s.executeQuery(sql);
+
+        int rowCount = 1;
+
+        while (hasil.next()) {
+            rowCount++;
+        }
+
+        return rowCount;
+    }
+    
+    public void insert(Customer c) throws SQLException 
+    {
+        int id_customer = this.countID();
+        String nama = c.getNama();
+        String alamat = c.getAlamat();
+        String noTelp = c.getNoTelp();
+
+        String sql = "INSERT INTO customer VALUES (" + id_customer + ",'" + nama + "','" + alamat + "','" + noTelp + "')";
+
+        Statement s = this.koneksi.createStatement();
+
+        s.executeUpdate(sql);
+    }
 }
