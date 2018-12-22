350769b6f08880ef2f26e326f7d25299d14adb02
bluohazard <greggygf@gmail.com>
2018-12-22 15:34:45 +0700

    modified entity customer

--- src/entity/Customer.java
+++ src/entity/Customer.java
@@ -2,5 +2,46 @@ package entity;
 
 public class Customer 
 {
+    private int id_customer;
+    private String nama, alamat, noTelp;
 
+    public int getId_customer() 
+    {
+        return id_customer;
+    }
+
+    public void setId_customer(int id_customer) 
+    {
+        this.id_customer = id_customer;
+    }
+
+    public String getNama() 
+    {
+        return nama;
+    }
+
+    public void setNama(String nama) 
+    {
+        this.nama = nama;
+    }
+
+    public String getAlamat() 
+    {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) 
+    {
+        this.alamat = alamat;
+    }
+
+    public String getNoTelp()
+    {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) 
+    {
+        this.noTelp = noTelp;
+    }
 }
