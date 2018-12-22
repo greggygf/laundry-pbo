f29e320f0b98de2e23c5772a9eef086aa3e47e41
bluohazard <greggygf@gmail.com>
2018-12-23 02:04:02 +0700

    modified

--- src/form/FormMenuCustomer.java
+++ src/form/FormMenuCustomer.java
@@ -182,7 +182,7 @@ public class FormMenuCustomer extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         dispose();
         
-        new FormAddJenisLaundry().setVisible(true);
+        new FormRiwayatTransaksi().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
