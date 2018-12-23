e9e2ace1dc9a9a9adefe34c79b8ebea3f0061741
bluohazard <greggygf@gmail.com>
2018-12-24 01:21:59 +0700

    modified main

--- src/main/main.java
+++ src/main/main.java
@@ -1,9 +1,13 @@
 package main;
 
+import form.FormLogin;
+
 public class main 
 {
     public static void main(String[] args) 
     {
-              
+        FormLogin fl = new FormLogin();
+        
+        fl.setVisible(true);
     }
 }
