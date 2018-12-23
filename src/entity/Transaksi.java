cf767a8fdefdd2be4ede8a5f469d84a28e140771
bluohazard <greggygf@gmail.com>
2018-12-23 20:10:51 +0700

    modified getter setter

--- src/entity/Transaksi.java
+++ src/entity/Transaksi.java
@@ -4,7 +4,7 @@ import java.sql.Date;
 
 public class Transaksi 
 {
-    private int id_transaksi, id_tarif, berat,total;
+    private int id_transaksi, id_tarif,id_jenis_pakaian,id_jenis_laundry, berat,biaya,total;
     private String nama, nama_jenis_laundry, nama_jenis_pakaian;
     private Date tgl;
     
@@ -12,10 +12,34 @@ public class Transaksi
     {
         
     }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
     
     public int getId_transaksi() 
     {
         return id_transaksi;
+    }
+
+    public int getId_jenis_pakaian() {
+        return id_jenis_pakaian;
+    }
+
+    public void setId_jenis_pakaian(int id_jenis_pakaian) {
+        this.id_jenis_pakaian = id_jenis_pakaian;
+    }
+
+    public int getId_jenis_laundry() {
+        return id_jenis_laundry;
+    }
+
+    public void setId_jenis_laundry(int id_jenis_laundry) {
+        this.id_jenis_laundry = id_jenis_laundry;
     }
 
     public void setId_transaksi(int id_transaksi) 
